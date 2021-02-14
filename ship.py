64c6e3d35321bfb2ba9d00ab54aacaaa58152f1f
Pratiksha Kapse <pratiksha@Pratikshas-MacBook-Pro.local>
2021-02-14 03:56:12 -0800

    move ship left and right by ship speed

--- ship.py
+++ ship.py
@@ -3,10 +3,24 @@ class Ship:
 
     def __init__(self,ai_game):
         self.screen=ai_game.screen
+        self.settings=ai_game.settings
         self.screen_rect =ai_game.screen.get_rect()
 
         self.image=pygame.image.load('images/ship.bmp')
         self.rect=self.image.get_rect()
         self.rect.midbottom=self.screen_rect.midbottom
+        
+        self.x=float(self.rect.x)
+
+        self.moving_right=False
+        self.moving_left=True
+        self.ship_speed =1.5
+    
+    def update(self):
+        if self.moving_right:
+            self.rect.x+=self.settings.ship_speed
+        if self.moving_left:
+            self.rect.x-=self.settings.ship_speed
+        self.rect.x=self.x
     def blittme(self):
         self.screen.blit(self.image,self.rect)
